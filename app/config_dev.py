1234059c637fe9242a886a8fe467f7aeb70f8c28
Dhirender Singh Negi <dhirender.negi@constems-ai.com>
2019-10-09 01:56:40 +0530

    removing debugging

--- app/config_dev.py
+++ app/config_dev.py
@@ -4,7 +4,7 @@ from app.config_common import *
 
 
 # DEBUG can only be set to True in a development environment for security reasons
-DEBUG = True
+DEBUG = False
 
 # Secret key for generating tokens
 SECRET_KEY = 'houdini'
