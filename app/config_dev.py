b30b415de3924c2bba41b84241d7f39272d10895
Dhirender Singh Negi <dhirender.negi@constems-ai.com>
2019-10-08 17:12:42 +0530

    change in debug

--- app/config_dev.py
+++ app/config_dev.py
@@ -4,7 +4,7 @@ from app.config_common import *
 
 
 # DEBUG can only be set to True in a development environment for security reasons
-DEBUG = False
+DEBUG = True
 
 # Secret key for generating tokens
 SECRET_KEY = 'houdini'
@@ -28,7 +28,7 @@ ADMINS = ['flask.boilerplate@gmail.com']
 # Number of times a password is hashed
 BCRYPT_LOG_ROUNDS = 12
 
-LOG_LEVEL = logging.INFO
+LOG_LEVEL = logging.DEBUG
 LOG_FILENAME = 'activity.log'
 LOG_MAXBYTES = 1024
 LOG_BACKUPS = 2
