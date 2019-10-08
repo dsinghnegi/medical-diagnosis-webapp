3045638cb719e76cd1d19e6956ef5483621c5933
Dhirender Singh Negi <dhirender.negi@constems-ai.com>
2019-10-09 01:20:57 +0530

    fix login issues

--- app/models.py
+++ app/models.py
@@ -28,10 +28,10 @@ class User(db.Model, UserMixin):
 
     @password.setter
     def password(self, plaintext):
-        self._password = bcrypt.generate_password_hash(plaintext.decode('utf-8'))
+        self._password = bcrypt.generate_password_hash(plaintext)
 
     def check_password(self, plaintext):
-        return bcrypt.check_password_hash(self.password,plaintext.encode('utf-8'))
+        return bcrypt.check_password_hash(self.password,plaintext)
 
     def get_id(self):
         return self.email
